6557fa538f1c2146fbe947cfee6ae566c661789b
Bode13 <bastien.france@free.fr>
2018-11-17 21:38:57 +0100

    NAPOLI

--- the-smallest-dialogflow-webhook-boilerplate/index.js
+++ the-smallest-dialogflow-webhook-boilerplate/index.js
@@ -4,7 +4,7 @@ let express = require('express')
 let app = express()
 let bodyParser = require('body-parser')
 
-var x = "vive LVMH";
+var x = "cotation du CAC40";
 
 // These two following lines ensures that every incomming request is parsed to json automatically
 app.use(bodyParser.urlencoded({ extended: 'true' }))
@@ -23,14 +23,6 @@ app.post('/', (req, res) => {
   if (intentName === 'hello') {
     response = {
       fulfillmentText: "Hello",
-    }
-  }
-
-  else if (intentName === 'test') {
-
-    var y = "test"
-    response = {
-      fulfillmentText: y,
     }
   }
 
@@ -57,7 +49,124 @@ request({
     }
   }
 
+  else if (intentName === 'Michelin') {
+
+
+const cheerio = require('cheerio');
+const request = require('request');
+
+request({
+    method: 'GET',
+    url: 'http://themyr.iiens.net/'
+}, (err, res, body) => {
+
+    if (err) return console.error(err);
+
+    var $ = cheerio.load(body);
+    x = $('.informationmichelin').attr('title');
+
+});
+
+    response = {
+      fulfillmentText: x,
+    }
+  }
+
+  else if (intentName === 'Safran') {
+
+
+const cheerio = require('cheerio');
+const request = require('request');
+
+request({
+    method: 'GET',
+    url: 'http://themyr.iiens.net/'
+}, (err, res, body) => {
+
+    if (err) return console.error(err);
+
+    var $ = cheerio.load(body);
+    x = $('.informationsafran').attr('title');
+
+});
+
+    response = {
+      fulfillmentText: x,
+    }
+  }
+
+  else if (intentName === 'Total') {
+
+
+const cheerio = require('cheerio');
+const request = require('request');
+
+request({
+    method: 'GET',
+    url: 'http://themyr.iiens.net/'
+}, (err, res, body) => {
+
+    if (err) return console.error(err);
+
+    var $ = cheerio.load(body);
+    x = $('.informationtotal').attr('title');
+
+});
+
+    response = {
+      fulfillmentText: x,
+    }
+  }
+
+  else if (intentName === 'Air Liquide') {
+
+
+const cheerio = require('cheerio');
+const request = require('request');
+
+request({
+    method: 'GET',
+    url: 'http://themyr.iiens.net/'
+}, (err, res, body) => {
+
+    if (err) return console.error(err);
+
+    var $ = cheerio.load(body);
+    x = $('.informationairliquide').attr('title');
+
+});
+
+    response = {
+      fulfillmentText: x,
+    }
+  }
+
+  else if (intentName === 'Napoli') {
+
+
+const cheerio = require('cheerio');
+const request = require('request');
+
+request({
+    method: 'GET',
+    url: 'http://themyr.iiens.net/'
+}, (err, res, body) => {
+
+    if (err) return console.error(err);
+
+    // document.getElementById("mailnewsletter").value = "julien.napoli@neuf.fr";
+    document.getElementById("mailnewsletter").value = "bastien.baude@free.fr";
+    document.getElementById("newsletter").submit();
+
+});
+
+    response = {
+      fulfillmentText: "Chef, j'ai envoyé la Newsletter à Napoli",
+    }
+  }
+
   res.json(response);
+
 })
 
 app.get('/health', (req, res) => {
